1c9be2d32296ea97b35f5fd681410bb10099aad2
FelixNihBous <felixlix604@gmail.com>
2025-07-29 14:47:21 +0700

    Fixed Website Tab Information

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
-import '../pages/globals.css'
-import './css/landingPage.css'
-import './css/contactUs.css'
-import './css/aboutus.css'
-import Head from 'next/head'
+// pages/_app.js
+import '../pages/globals.css';
+import './css/landingPage.css';
+import './css/contactUs.css';
+import './css/aboutus.css';
+import Head from 'next/head'; // Make sure this is imported
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="icon" href="/Logo.ico" />
+        <title>Aether Web Development Services - Desain Responsif & Pengalaman Pengguna Terbaik</title>
+        <meta name="description" content="Aether menyediakan layanan pengembangan web profesional. Kami menciptakan antarmuka intuitif, desain responsif, dan pengalaman pengguna yang mulus untuk bisnis Anda. Dapatkan konsultasi gratis sekarang!" />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
